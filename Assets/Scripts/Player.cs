604726b63ccf3504c08d03a4aa8169fae583092d
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-02-22 10:25:18 +0200

    Added player movement and width constraints

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 15f;
+    public float mapWidth = 9f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    } 
+
+    private void FixedUpdate() // https://answers.unity.com/questions/10993/whats-the-difference-between-update-and-fixedupdat.html
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        Vector2 newPosition = new Vector2(rb.position.x + x, 0);
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+
+        rb.MovePosition(newPosition);
+    }
+}
