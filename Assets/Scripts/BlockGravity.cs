30fb33a2b3c7d0c39ceb435aa8dc720cc22f48d6
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-02-22 12:12:48 +0200

    Added gravity scaling

---
+++ Assets/Scripts/BlockGravity.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BlockGravity : MonoBehaviour
+{
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad / 20f;
+        Debug.Log(GetComponent<Rigidbody2D>().gravityScale);
+    }
+
+
+}
